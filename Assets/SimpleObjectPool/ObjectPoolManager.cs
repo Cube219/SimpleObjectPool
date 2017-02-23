c32ce43ba2a3396c7efb6350b95bb59a7124104b
임형진(Hyeong-Jin Lim) <max804@naver.com>
2017-02-23 15:06:46 +0900

    프로젝트 초기화(Initialize Project)

---
+++ Assets/SimpleObjectPool/ObjectPoolManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+namespace Cube219.SimpleObjectPool
+{
+	public class ObjectPoolManager:MonoBehaviour
+	{
+		private Type managedType_;
+		public Type managedType { get { return managedType_; } }
+
+		private List<PoolObject> poolObjects_;
+	}
+}
---
+++ Assets/SimpleObjectPool/SimpleObjectPool.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Cube219.SimpleObjectPool
+{
+	public class SimpleObjectPool
+	{
+		private static List<ObjectPoolManager> managers_ = new List<ObjectPoolManager>();
+
+		public static ObjectPoolManager GetManager(Type poolObjectType)
+		{
+			
+		}
+	}
+}
